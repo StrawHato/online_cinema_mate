5e19854de56c895e7d0c2ebefc13d65e80f88bea
Maksym Pakula <gargantadoto@gmail.com>
2026-06-24 15:56:09 +0200

    Add login route

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config.dependencies import (
     get_accounts_email_notificator,
+    get_jwt_auth_manager,
 )
+from src.config.settings import Settings, get_settings
 from src.database.session import get_db
 from src.notifications.interfaces import EmailSenderInterface
 from src.schemas.accounts import (
@@ -11,9 +13,12 @@ from src.schemas.accounts import (
     PasswordResetCompleteRequestSchema,
     PasswordResetRequestSchema,
     UserActivationRequestSchema,
+    UserLoginRequestSchema,
+    UserLoginResponseSchema,
     UserRegistrationRequestSchema,
     UserRegistrationResponseSchema,
 )
+from src.security.interfaces import JWTAuthManagerInterface
 from src.services.accounts import AccountsService
 
 router = APIRouter(prefix="/accounts", tags=["Accounts"])
@@ -89,4 +94,25 @@ async def reset_password(
         data=data,
         db=db,
         email_sender=email_sender,
+    )
+
+
+@router.post(
+    "/login/",
+    response_model=UserLoginResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def login_user(
+    login_data: UserLoginRequestSchema,
+    db: AsyncSession = Depends(get_db),
+    settings: Settings = Depends(get_settings),
+    jwt_manager: JWTAuthManagerInterface = Depends(
+        get_jwt_auth_manager
+    ),
+) -> UserLoginResponseSchema:
+    return await AccountsService.login(
+        login_data=login_data,
+        db=db,
+        settings=settings,
+        jwt_manager=jwt_manager,
     )
