0f073f56f32f67ef00f36b12790d529cc82cd545
Maksym Pakula <gargantadoto@gmail.com>
2026-06-24 16:19:56 +0200

    Add chenge password service

--- src/services/accounts.py
+++ src/services/accounts.py
@@ -28,6 +28,9 @@ from src.schemas.accounts import (
     UserLoginResponseSchema,
     TokenRefreshRequestSchema,
     TokenRefreshResponseSchema,
+    ChangePasswordRequestSchema,
+    LogoutRequestSchema,
+    ResendActivationRequestSchema,
 )
 from src.notifications.interfaces import EmailSenderInterface
 from src.security.interfaces import JWTAuthManagerInterface
@@ -406,4 +409,38 @@ class AccountsService:
 
         return TokenRefreshResponseSchema(
             access_token=new_access_token
+        )
+
+    @staticmethod
+    async def change_password(
+            user_id: int,
+            data: ChangePasswordRequestSchema,
+            db: AsyncSession,
+    ) -> MessageResponseSchema:
+
+        stmt = select(UserModel).where(
+            UserModel.id == user_id
+        )
+
+        result = await db.execute(stmt)
+        user = result.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found."
+            )
+
+        if not user.verify_password(data.old_password):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Old password is incorrect."
+            )
+
+        user.password = data.new_password
+
+        await db.commit()
+
+        return MessageResponseSchema(
+            message="Password changed successfully."
         )
