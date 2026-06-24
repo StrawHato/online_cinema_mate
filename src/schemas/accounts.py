ebd324933b955d167d56189b2ec2587e55a5bfb9
Maksym Pakula <gargantadoto@gmail.com>
2026-06-24 15:27:18 +0200

    Add Base Email password schema

---
+++ src/schemas/accounts.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, field_validator
+
+from src.database.validators import accounts as accounts_validators
+
+
+class BaseEmailPasswordSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        return value.lower()
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        return accounts_validators.validate_password_strength(value)
