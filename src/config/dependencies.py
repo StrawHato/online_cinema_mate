f09f9c1f8217f20683a3065dd851c90a33de271b
Maksym Pakula <gargantadoto@gmail.com>
2026-06-24 15:05:45 +0200

    Add email notificator dependency

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,5 +1,7 @@
 from functools import lru_cache
 
+from notifications import EmailSenderInterface
+from notifications.emails import EmailSender
 from src.config.settings import get_settings
 from src.security.interfaces import JWTAuthManagerInterface
 from src.security.token_manager import JWTAuthManager
@@ -13,4 +15,22 @@ def get_jwt_auth_manager() -> JWTAuthManagerInterface:
         secret_key_access=settings.SECRET_KEY_ACCESS,
         secret_key_refresh=settings.SECRET_KEY_REFRESH,
         algorithm=settings.JWT_SIGNING_ALGORITHM,
+    )
+
+
+@lru_cache
+def get_accounts_email_notificator() -> EmailSenderInterface:
+    settings = get_settings()
+
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
+        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,
     )
