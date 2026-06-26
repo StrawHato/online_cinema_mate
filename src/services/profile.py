183c795cf4a1b12eb0462a91c58f379d940f4c5e
Maksym Pakula <gargantadoto@gmail.com>
2026-06-26 13:28:55 +0200

    add profile update function

--- src/services/profile.py
+++ src/services/profile.py
@@ -1,7 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.database.models.accounts import UserModel
 from src.schemas.profile import (
+    ProfileUpdateRequestSchema,
     ProfileResponseSchema,
 )
+from src.storages.interfaces import StorageInterface
 
 
 class ProfileService:
@@ -10,5 +14,45 @@ class ProfileService:
         current_user: UserModel,
     ) -> ProfileResponseSchema:
         profile = current_user.profile
+
+        return ProfileResponseSchema.model_validate(profile)
+
+    @staticmethod
+    async def update_profile(
+            db: AsyncSession,
+            storage: StorageInterface,
+            current_user: UserModel,
+            profile_data: ProfileUpdateRequestSchema,
+    ) -> ProfileResponseSchema:
+        profile = current_user.profile
+
+        data = profile_data.model_dump(
+            exclude_unset=True,
+            exclude={"avatar"},
+        )
+
+        for field, value in data.items():
+            setattr(profile, field, value)
+
+        if profile_data.avatar is not None:
+            avatar_bytes = await profile_data.avatar.read()
+
+            avatar_key = (
+                f"avatars/"
+                f"{current_user.id}_"
+                f"{profile_data.avatar.filename}"
+            )
+
+            await storage.upload_file(
+                file_name=avatar_key,
+                file_data=avatar_bytes,
+                content_type=profile_data.avatar.content_type
+                             or "application/octet-stream",
+            )
+
+            profile.avatar = avatar_key
+
+        await db.commit()
+        await db.refresh(profile)
 
         return ProfileResponseSchema.model_validate(profile)
