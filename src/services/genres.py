12e07d5c3314d5baf47553f49e5a0871a6855e40
Maksym Pakula <gargantadoto@gmail.com>
2026-06-27 12:29:50 +0200

    add update and delete services

--- src/services/genres.py
+++ src/services/genres.py
@@ -10,6 +10,7 @@ from src.schemas.genres import (
     GenreCreateRequestSchema,
     GenreDetailResponseSchema,
     GenreResponseSchema,
+    GenreUpdateRequestSchema,
 )
 
 
@@ -107,3 +108,98 @@ class GenreService:
             )
             for row in result.all()
         ]
+
+    @staticmethod
+    async def update_genre(
+            genre_id: int,
+            genre_data: GenreUpdateRequestSchema,
+            db: AsyncSession,
+    ) -> GenreDetailResponseSchema:
+
+        stmt = select(GenreModel).where(
+            GenreModel.id == genre_id
+        )
+
+        result = await db.execute(stmt)
+
+        genre = result.scalar_one_or_none()
+
+        if genre is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Genre not found.",
+            )
+
+        if genre_data.name is not None:
+
+            stmt = (
+                select(GenreModel)
+                .where(
+                    GenreModel.name == genre_data.name,
+                    GenreModel.id != genre_id,
+                )
+            )
+
+            result = await db.execute(stmt)
+
+            if result.scalar_one_or_none():
+                raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail="Genre already exists.",
+                )
+
+            genre.name = genre_data.name
+
+        await db.commit()
+        await db.refresh(genre)
+
+        return GenreDetailResponseSchema.model_validate(
+            genre
+        )
+
+    @staticmethod
+    async def delete_genre(
+            genre_id: int,
+            db: AsyncSession,
+    ) -> None:
+
+        stmt = (
+            select(GenreModel)
+            .where(
+                GenreModel.id == genre_id
+            )
+        )
+
+        result = await db.execute(stmt)
+
+        genre = result.scalar_one_or_none()
+
+        if genre is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Genre not found.",
+            )
+
+        stmt = (
+            select(func.count())
+            .select_from(MovieGenresTable)
+            .where(
+                MovieGenresTable.c.genre_id == genre_id
+            )
+        )
+
+        result = await db.execute(stmt)
+
+        movies_count = result.scalar_one()
+
+        if movies_count > 0:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=(
+                    "Genre cannot be deleted because "
+                    "it is assigned to one or more movies."
+                ),
+            )
+
+        await db.delete(genre)
+        await db.commit()
