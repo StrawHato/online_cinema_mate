8cf638bcffdb7710c73d8e74231406badb88e679
Maksym Pakula <gargantadoto@gmail.com>
2026-06-27 12:23:43 +0200

    add create/update and detail genre schemas

--- src/schemas/genres.py
+++ src/schemas/genres.py
@@ -1,10 +1,27 @@
 from pydantic import BaseModel
 
 
+class GenreCreateRequestSchema(BaseModel):
+    name: str
+
+
+class GenreUpdateRequestSchema(BaseModel):
+    name: str | None = None
+
+
 class GenreResponseSchema(BaseModel):
     id: int
     name: str
     movies_count: int
+
+    model_config = {
+        "from_attributes": True,
+    }
+
+
+class GenreDetailResponseSchema(BaseModel):
+    id: int
+    name: str
 
     model_config = {
         "from_attributes": True,
