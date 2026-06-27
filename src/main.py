0155a3fdc92a76316bd6dd331cb8840f0010ea10
Maksym Pakula <gargantadoto@gmail.com>
2026-06-27 12:37:20 +0200

    Add stars router and register it in main app with genres

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-from src.routes import accounts_router, profile_router, movies_router
+from src.routes import (
+    accounts_router,
+    profile_router,
+    movies_router,
+    genres_router,
+    actors_router
+)
 
 app = FastAPI(
     title="Online Cinema Mate",
@@ -12,3 +18,5 @@ api_version_prefix = "/api/v1"
 app.include_router(accounts_router, prefix=f"{api_version_prefix}")
 app.include_router(profile_router, prefix=f"{api_version_prefix}")
 app.include_router(movies_router, prefix=f"{api_version_prefix}")
+app.include_router(genres_router, prefix=f"{api_version_prefix}")
+app.include_router(actors_router, prefix=f"{api_version_prefix}")
