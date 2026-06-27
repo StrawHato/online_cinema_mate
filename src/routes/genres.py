adaba4cb8b6b01efe1475102f0ea1199e0d82c0b
Maksym Pakula <gargantadoto@gmail.com>
2026-06-27 12:26:31 +0200

    add post and detail genre routes

--- src/routes/genres.py
+++ src/routes/genres.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter,
+    Depends,
+    status,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.schemas.genres import GenreResponseSchema
-from src.services.genres import GenreService
+from src.database.models.accounts import UserModel
 from src.database.session import get_db
+from src.schemas.genres import (
+    GenreCreateRequestSchema,
+    GenreDetailResponseSchema,
+    GenreResponseSchema,
+)
+from src.security.http import get_current_admin
+from src.services.genres import GenreService
 
 
 router = APIRouter(
     prefix="/genres",
-    tags=["Genre"],
+    tags=["Genres"],
 )
 
 
@@ -22,3 +32,35 @@ async def get_genres(
 ) -> list[GenreResponseSchema]:
 
     return await GenreService.get_genres(db)
+
+
+@router.get(
+    "/{genre_id}/",
+    response_model=GenreDetailResponseSchema,
+)
+async def get_genre(
+    genre_id: int,
+    db: AsyncSession = Depends(get_db),
+):
+
+    return await GenreService.get_genre(
+        genre_id=genre_id,
+        db=db,
+    )
+
+
+@router.post(
+    "/",
+    response_model=GenreDetailResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_genre(
+    genre_data: GenreCreateRequestSchema,
+    db: AsyncSession = Depends(get_db),
+    current_user: UserModel = Depends(get_current_admin),
+):
+
+    return await GenreService.create_genre(
+        db=db,
+        genre_data=genre_data,
+    )
