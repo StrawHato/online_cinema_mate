e5ba54d9f4dbb2601ef3bd2f6dfe0359dea6045e
Maksym Pakula <gargantadoto@gmail.com>
2026-06-28 12:20:20 +0200

    Add Payment Response schema

--- src/schemas/payments.py
+++ src/schemas/payments.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from decimal import Decimal
 
 from pydantic import BaseModel
 
+from src.database.models.payments import PaymentStatusEnum
 from src.schemas.orders import OrderMovieResponseSchema
 
 
@@ -10,3 +12,15 @@ class PaymentItemResponseSchema(BaseModel):
     price_at_payment: Decimal
 
     order_item: OrderMovieResponseSchema
+
+
+class PaymentResponseSchema(BaseModel):
+    uuid: str
+
+    created_at: datetime
+
+    status: PaymentStatusEnum
+
+    amount: Decimal
+
+    items: list[PaymentItemResponseSchema]
